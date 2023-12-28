e287a534ffdcebbbe9f7859db49faded09b646fc
hnnynh <hnnynh125@gmail.com>
2023-12-29 03:08:45 +0900

    feat: 카카오 로그인, 회원가입 구현

--- config/baseResponseStatus.js
+++ config/baseResponseStatus.js
@@ -1,1 +1,2 @@
-export const SUCCESS = { isSuccess: true, code: 200, message: "success" };
+export const SUCCESS = { code: "200", message: "success" };
+export const BAD_REQUEST = { status: "400", message: "Bad Request" };
--- src/app/User/userService.js
+++ src/app/User/userService.js
@@ -1,1 +1,8 @@
 // 생성, 업데이트, 삭제 관련 로직
+
+import * as userDao from "./userDao.js";
+
+export const createUser = async (userId, userName) => {
+  const newUser = await userDao.createNewUser(userId, userName);
+  return newUser;
+};
